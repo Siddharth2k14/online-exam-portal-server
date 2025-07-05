c89485c410d002be021378ec801f9bd7f3e0013c
Siddharth2k14 <siddharthsingh2k@gmail.com>
2025-07-05 21:01:56 +0530

    fix: update API endpoint in Online-Exam-Portal to use localhost for local development and adjust server listening log in index.js

--- api/index.js
+++ api/index.js
@@ -9,29 +9,22 @@ import serverless from 'serverless-http';
 import questionRoute from '../Routes/QuestionRoute.js'
 
 dotenv.config();
+const PORT = 3000
 
 const app = express();
-const PORT = 3000;
 
-// Connect to MongoDB once per function instance
+// Connect to MongoDB
 connectQuesDB();
 connectAuthDB();
 
-// Allow only your deployed frontend origin
-app.use(cors({
-    origin: "https://online-exam-portal-client.vercel.app/",
-    credentials: true
-}));
-
-app.get('/', (req, res) => {
-    console.log('API is running...');
-    res.send('API is running...');
-});
-
+app.use(cors());
 app.use(express.json());
 app.use('/api/questions', questionRoute);
 app.use('/api/auth', authRoute);
 
+// ❌ Remove app.listen()
+// ✅ Instead, export a handler
+// export const handler = serverless(app);
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+    console.log(`Server is listening at ${PORT}`)
+})
