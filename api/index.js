0e36f3bfce6a162ad25115bb56bacbdb516ffe9e
Siddharth2k14 <siddharthsingh2k@gmail.com>
2025-07-05 20:48:53 +0530

    Remove server code from index.js and update dependencies in package.json and package-lock.json. Adjust API endpoint in Online-Exam-Portal to use the production URL. Clean up vercel.json formatting.

---
+++ api/index.js
@@ -0,0 +1,37 @@
+// api/index.js
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectQuesDB from '../DB/QuestionDB.js'
+import authRoute from '../Routes/AuthRoute.js'
+import connectAuthDB from '../DB/AuthDB.js'
+import serverless from 'serverless-http';
+import questionRoute from '../Routes/QuestionRoute.js'
+
+dotenv.config();
+
+const app = express();
+const PORT = 3000;
+
+// Connect to MongoDB once per function instance
+connectQuesDB();
+connectAuthDB();
+
+// Allow only your deployed frontend origin
+app.use(cors({
+    origin: "https://online-exam-portal-client.vercel.app/",
+    credentials: true
+}));
+
+app.get('/', (req, res) => {
+    console.log('API is running...');
+    res.send('API is running...');
+});
+
+app.use(express.json());
+app.use('/api/questions', questionRoute);
+app.use('/api/auth', authRoute);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
