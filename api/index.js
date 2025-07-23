d94600d1545818051e11751a36692955990fad12
Siddharth2k14 <siddharthsingh2k@gmail.com>
2025-07-23 16:40:20 +0530

    Changing the UI

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ app.use('/api/auth', AuthRoute);
 
 // ❌ Remove app.listen()
 // ✅ Instead, export a handler
-// export const handler = serverless(app);
-app.listen(PORT, () => {
-    console.log(`Server is listening at ${PORT}`)
-})
+export const handler = serverless(app);
+// app.listen(PORT, () => {
+//     console.log(`Server is listening at ${PORT}`)
+// })
